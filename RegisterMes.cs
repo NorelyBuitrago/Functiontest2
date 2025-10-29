fc1d2c0f440ea5df751dfc3864ebf98236ec14f7
LAPTOP-0CV17E1M\UserMaquinaVirtual <norelibuitrago@hotmail.com>
2025-10-29 18:22:47 -0500

    the function is created

---
+++ RegisterMes.cs
@@ -0,0 +1,21 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+public class RegisterMes
+{
+    private readonly ILogger<RegisterMes> _logger;
+    public RegisterMes(ILogger<RegisterMes> logger) => _logger = logger;
+
+    [Function("RegisterMes")]
+    public async Task<HttpResponseData> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "RegisterMes")]
+        HttpRequestData req)
+    {
+        var res = req.CreateResponse(HttpStatusCode.OK);
+        res.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+        await res.WriteStringAsync("Hermosa Norely lograste");
+        return res;
+    }
+}
